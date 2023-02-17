e597654110cdcc1d3153524621d92224371596a2
jacksonhill507 <jacksonchurchill507@gmail.com>
2023-02-17 21:36:26 +0100

    my commit on rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-''' rotates a 2D matrix '''
+'''2D matrix'''
 
 
 def rotate_2d_matrix(matrix):
-    '''
-        rotates 2d matrix 90 degrees clockwise
-        rotation happens in place
-        args:
-            matrix
-    '''
-    rows = len(matrix)
-    columns = rows - 1
+    '''rotates a 2d matrix 90° clockwise
+    Returns: Nothing'''
+    left, right = 0, len(matrix) - 1
 
-    for i in range(0, int(rows / 2)):
-        for j in range(i, columns - i):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[columns - j][i]
-            matrix[columns - j][i] = matrix[columns - i][columns - j]
-            matrix[columns - i][columns - j] = matrix[j][columns - i]
-            matrix[j][columns - i] = tem
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
